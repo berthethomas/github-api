614906ee82c258c018fd2d49335518fc7d9a7548
Thomas Berthé <berthe.thomas01@gmail.com>
2018-05-31 13:53:35 +0200

    client web done

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,34 @@
 $(document).ready(function () {
     checkAuthentification();
+    livres();
+    auteurs();
 });
 
-$(document).on('click', '#tab-auteurs', function () {
+$(document).on('click', '#reset-livre', function () {
+    $('#livre-id').val("");
+    livres();
+});
+
+$(document).on('click', '#reset-auteur', function () {
+    $('#auteur-id').val("");
     auteurs();
 });
 
 $(document).on('click', '#recherche-auteur', function () {
     auteur();
+});
+
+$(document).on('click', '#recherche-livre', function () {
+    livre();
+});
+
+$(document).on('click', '.btn-add-commentaire', function () {
+    var livreId = $(this).data('livre-id');
+    $('#livre-id-commentaire').val(livreId);
+});
+
+$(document).on('click', '#add-commentaire-validation', function () {
+    addCommentaire();
 });
 
 function authentification() {
@@ -52,7 +73,7 @@ function auteurs() {
         },
         data: JSON.stringify({
             query: `query {
-                        auteurs() { 
+                        auteurs { 
                             id,
                             nom,
                             prenom,
@@ -173,6 +194,213 @@ function auteur() {
                             '</td>' +
                             '<td>' +
                             livres +
+                            '</td>' +
+                            '</tr>');
+                }
+            }
+        });
+    }
+}
+
+function livres() {
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:8000/graphql",
+        contentType: "application/json",
+        headers: {
+            token: $.cookie('token')
+        },
+        data: JSON.stringify({
+            query: `query {
+                        livres {
+                            id,
+                            titre,
+                            genre,
+                            date_parution,
+                            prix,
+                            auteur {
+                              nom,
+                              prenom
+                            },
+                            commentaires {
+                              texte,
+                              note
+                            }
+                        }
+                    }`
+        }),
+        success: function (data) {
+            var table = $('#table-livres').children('tbody');
+            table.empty();
+
+            if (data.length === 0) {
+                table.append('<tr><td colspan="7">Aucuns résultats</td></tr>');
+            } else {
+                $.each(data.data.livres, function (key, value) {
+                    if (value.commentaires.length === 0) {
+                        var commentaires = "Aucuns Commentaires";
+                    } else {
+                        var commentaires = "<ul>";
+
+                        $.each(value.commentaires, function (key, commentaire) {
+                            commentaires += "<li>" + commentaire.texte + " (" + commentaire.note + ")</li>";
+                        });
+
+                        commentaires += "</ul>";
+                    }
+
+                    table.append(
+                            '<tr>' +
+                            '<td>' +
+                            value.id +
+                            '</td>' +
+                            '<td>' +
+                            value.titre +
+                            '</td>' +
+                            '<td>' +
+                            value.genre +
+                            '</td>' +
+                            '<td>' +
+                            $.format.date(value.date_parution, "dd/MM/yyyy") +
+                            '</td>' +
+                            '<td>' +
+                            value.prix +
+                            '</td>' +
+                            '<td>' +
+                            value.auteur.prenom + " " + value.auteur.nom +
+                            '</td>' +
+                            '<td>' +
+                            commentaires +
+                            '</td>' +
+                            '<td>' +
+                            '<a href="#add-commentaire" data-toggle="modal" data-target="#add-commentaire" class="btn btn-sm btn-success btn-add-commentaire" data-livre-id="' + value.id + '"><i class="glyphicon glyphicon-plus"></i> Ajouter un commentaire</a>' +
+                            '</td>' +
+                            '</tr>');
+                });
+            }
+        }
+    });
+}
+
+function addCommentaire() {
+    var livreId = $('#livre-id-commentaire').val();
+    var texte = $('#texte-commentaire').val();
+    var note = $('#note-commentaire').val();
+
+    if (livreId !== null && livreId !== "" && texte !== null && texte !== "" && note !== null && note !== "") {
+
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:8000/graphql",
+            contentType: "application/json",
+            headers: {
+                token: $.cookie('token')
+            },
+            data: JSON.stringify({
+                query: `mutation ($texte: String!, $note: Float!, $livre_id: ID!) {
+                            addCommentaire(texte: $texte, note: $note, livre_id: $livre_id)
+                       }`,
+                variables: {
+                    "texte": texte,
+                    "note": parseFloat(note),
+                    "livre_id": parseInt(livreId)
+                }
+            }),
+            success: function (data) {
+                if (data.data.livre !== null && data.data.livre !== "") {
+                    livres();
+                    $('#add-commentaire').modal('hide');
+                } else {
+                    console.log(data.errors);
+                }
+            }
+        });
+    } else {
+        alert("Veuillez remplir le formulaire !");
+    }
+}
+
+function livre() {
+    var id = $('#livre-id').val();
+
+    if (id === null || id === "") {
+        alert("Vous devez rensigner un ID !");
+    } else {
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:8000/graphql",
+            contentType: "application/json",
+            headers: {
+                token: $.cookie('token')
+            },
+            data: JSON.stringify({
+                query: `query($id: Int!) {
+                        livre(id: $id) { 
+                            id,
+                            titre,
+                            genre,
+                            date_parution,
+                            prix,
+                            auteur {
+                              nom,
+                              prenom
+                            },
+                            commentaires {
+                              texte,
+                              note
+                            }
+                        }
+                    }`,
+                variables: {
+                    "id": parseInt(id)
+                }
+            }),
+            success: function (data) {
+                var table = $('#table-livres').children('tbody');
+                table.empty();
+
+                if (data.length === 0 || data.data.livre === null) {
+                    table.append('<tr><td colspan="7">Aucuns résultats pour votre recherche</td></tr>');
+                } else {
+                    var livre = data.data.livre;
+
+                    if (livre.commentaires.length === 0) {
+                        var commentaires = "Aucuns Commentaires";
+                    } else {
+                        var commentaires = "<ul>";
+
+                        $.each(livre.commentaires, function (key, commentaire) {
+                            commentaires += "<li>" + commentaire.texte + " (" + commentaire.note + ")</li>";
+                        });
+
+                        commentaires += "</ul>";
+                    }
+
+                    table.append(
+                            '<tr>' +
+                            '<td>' +
+                            livre.id +
+                            '</td>' +
+                            '<td>' +
+                            livre.titre +
+                            '</td>' +
+                            '<td>' +
+                            livre.genre +
+                            '</td>' +
+                            '<td>' +
+                            $.format.date(livre.date_parution, "dd/MM/yyyy") +
+                            '</td>' +
+                            '<td>' +
+                            livre.prix +
+                            '</td>' +
+                            '<td>' +
+                            livre.auteur.prenom + " " + livre.auteur.nom +
+                            '</td>' +
+                            '<td>' +
+                            commentaires +
+                            '</td>' +
+                            '<td>' +
+                            '<a href="#add-commentaire" data-toggle="modal" data-target="#add-commentaire" class="btn btn-sm btn-success btn-add-commentaire" data-livre-id="' + livre.id + '"><i class="glyphicon glyphicon-plus"></i> Ajouter un commentaire</a>' +
                             '</td>' +
                             '</tr>');
                 }
